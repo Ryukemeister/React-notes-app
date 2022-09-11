ade0690d5e1d45045cbca4166781468e1754097a
Ryuke <sahalrajiv-extc@atharvacoe.ac.in>
2022-09-11 20:51:34 +0530

    Apllying some more tailwind styless to the nav elements for responsiveness

--- notes/src/components/Header.jsx
+++ notes/src/components/Header.jsx
@@ -3,13 +3,13 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav className="bg-black flex text-white pl-5">
+    <nav className="bg-black flex text-white pl-2 lg:pl-6 drop-shadow-md">
       <div className="nav-logo"></div>
       <div className="nav-links">
-        <ul className="flex">
-          <li className="p-5">Home</li>
-          <li className="p-5">About</li>
-          <li className="p-5">Rate us</li>
+        <ul className="flex gap-7 py-4 lg:py-3 px-5 text-lg">
+          <li>Home</li>
+          <li>About</li>
+          <li>Rate us</li>
         </ul>
       </div>
     </nav>
